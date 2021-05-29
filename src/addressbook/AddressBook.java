05bcae71d45fef994167f69bfd2388e5593d77a4
Indrajeet <indrajeetseru@gmail.com>
2021-05-29 18:19:40 +0530

    Added Contact EditName in AddressBook

--- src/addressbook/AddressBook.java
+++ src/addressbook/AddressBook.java
@@ -28,14 +28,15 @@ public class AddressBook {
 		sc.nextLine();
 		System.out.println("Enter the Contact Number");
 		String Contact_Number = sc.nextLine();
-		System.out.println("Enter the Email address\n");
+		System.out.println("Enter the Email address");
 		String eMail = sc.nextLine();
 
 		AddressBook entry = new AddressBook(First_Name, Last_Name, Address, City, State, zip, Contact_Number, eMail);
 		entry.addressBook();
 	}
 
-	public AddressBook(String First_Name, String Last_Name, String Address, String City, String State, int zip, String Contact_Number, String eMail) {
+	public AddressBook(String First_Name, String Last_Name, String Address, String City, String State, int zip,
+			String Contact_Number, String eMail) {
 		this.First_Name = First_Name;
 		this.Last_Name = Last_Name;
 		this.Address = Address;
@@ -51,7 +52,7 @@ public class AddressBook {
 	}
 
 	public void addressBook() {
-		System.out.println("First Name: " + First_Name);
+		System.out.println("\nFirst Name: " + First_Name);
 		System.out.println("Last Name: " + Last_Name);
 		System.out.println("Address: " + Address);
 		System.out.println("City: " + City);
@@ -61,11 +62,39 @@ public class AddressBook {
 		System.out.println("Email Address: " + eMail);
 	}
 
+	public void editPerson() {
+		System.out.println("\nCross Check the details. if you want to edit Type '1' for Yes or '0' for No ");
+		Scanner sc = new Scanner(System.in);
+		int YN = sc.nextInt();
+		if (YN == 1) {
+			Scanner sc1 = new Scanner(System.in);
+			System.out.println("\nEnter the choice: What you Want to edit");
+			System.out.println("1:- First_Name \n2:- Last_Name");
+			int edit = sc1.nextInt();
+			switch (edit) {
+			case 1:
+				System.out.println("Enter the New Name What you want to Edit");
+				Scanner sc2 = new Scanner(System.in);
+				String FN = sc2.nextLine();
+				System.out.println("The New Name Entered is: " + FN);
+				break;
+			case 2:
+				System.out.println("Enter the New Name What you want to Edit");
+				Scanner sc3 = new Scanner(System.in);
+				String LN = sc3.nextLine();
+				System.out.println("The New Nmae Entered is: " + LN);
+				break;
+			}
+		} else {
+			System.out.println("###..Nothing to edit..###");
+		}
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println("..####..Welcome to Address Book Programm..####..");
 		AddressBook register = new AddressBook();
 		register.addContactDetails();
-
+		register.editPerson();
 	}
 }
